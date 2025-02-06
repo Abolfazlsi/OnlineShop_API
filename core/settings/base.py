0e8d3fb59a2a1e6232df73b54c898b60afb43e63
Abolfazl Shojaei <dga51547@gmail.com>
2025-02-06 13:46:45 +0330

    fix bug

--- core/settings/base.py
+++ core/settings/base.py
@@ -105,7 +105,7 @@ AUTH_USER_MODEL = "account.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework_simplejwt.authentication.JWTAuthentication"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 2
+    "PAGE_SIZE": 12
 }
 
 SIMPLE_JWT = {
